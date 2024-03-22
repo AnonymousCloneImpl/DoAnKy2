f03442349f0b60646b9aa81e40ae064a2784fdbc
huunghiadvc <mos98p@gmail.com>
2024-03-22 08:48:12 +0700

    update

--- backend/src/main/java/project/product/models/Pagination.java
+++ backend/src/main/java/project/product/models/Pagination.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import project.product.dto.ProductSummaryDto;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pagination {
+public class Pagination implements Serializable {
 	public static final int PAGE_SIZE = 15;
 	private int totalPageNumber;
 	private long totalElement;
--- backend/src/main/java/project/search/service/SearchServiceImpl.java
+++ backend/src/main/java/project/search/service/SearchServiceImpl.java
@@ -2,6 +2,7 @@ package project.search.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
