68b5aa32842c004425b17c3ddcb87b5b92c5567e
TH2302023 DT <dtranjack@gmail.com>
2024-04-02 19:43:33 +0700

    fixed a bit of controller

--- adminlte/app/Http/Controllers/ProductController.php
+++ adminlte/app/Http/Controllers/ProductController.php
@@ -173,10 +173,5 @@ class ProductController extends Controller
             toastr()->error('Product is not removed', 'Oops! Something went wrong!');
             return redirect()->back();
         }
-
-
-
-        return redirect()->back();
-
     }
 }
