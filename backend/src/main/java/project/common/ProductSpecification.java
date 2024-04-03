5bcb2fa22f763a62d04da5b06fdeee2da7dba42d
huunghiadvc <mos98p@gmail.com>
2024-04-03 09:12:42 +0700

    update

--- backend/src/main/java/project/common/ProductSpecification.java
+++ backend/src/main/java/project/common/ProductSpecification.java
@@ -1,7 +1,10 @@
 package project.common;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.*;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
@@ -11,12 +14,10 @@ import project.dto.search.RequestDto;
 import project.dto.search.SearchRequestDto;
 import project.entity.product.Producer;
 import project.entity.product.Product;
-import project.entity.product.ProductDetail;
 import project.entity.product.Stock;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Component
 @Slf4j(topic = "PRODUCT-SPECIFICATION")
@@ -39,43 +40,41 @@ public class ProductSpecification {
 
 	public Specification<Product> specificationBuilder(RequestDto requestDto) {
 		return (root, query, criteriaBuilder) -> {
-
-			// JOIN TABLE
-			Join<Product, ProductDetail> productDetailsJoin = root.join("productDetails", JoinType.INNER);
 			Join<Product, Producer> producerJoin = root.join("producer", JoinType.INNER);
-			Join<ProductDetail, Stock> stockJoin = productDetailsJoin.join("stock", JoinType.INNER);
-
+			Join<Product, Stock> stockJoin = root.join("stock", JoinType.INNER);
 			List<Predicate> predicateList = new ArrayList<>();
 
 			// CHECK COLUMN
 			for (SearchRequestDto searchRequestDto : requestDto.getSearchRequestDtoList()) {
 				String column = searchRequestDto.getColumn();
-				Path<Object> attributePath;
-
 				Predicate predicate;
+
 				switch (column) {
-					case "type" -> {
-						attributePath = root.get(column);
-						predicate = criteriaBuilder.equal(
-								attributePath.as(String.class),
-								searchRequestDto.getValue()
-						);
-						predicateList.add(predicate);
-					}
 
 					case "producer" -> {
-						attributePath = producerJoin.get("name");
+						// JOIN TABLE
 						predicate = criteriaBuilder.equal(
-								attributePath.as(String.class),
+								producerJoin.get("name"),
 								searchRequestDto.getValue()
 						);
 						predicateList.add(predicate);
 					}
 
-					case "cpuType", "ram", "hardDisk", "screenSize", "mouseConnectType", "keyboardConnectType" -> {
-						attributePath = productDetailsJoin.get(column);
+					case "type" -> {
 						predicate = criteriaBuilder.like(
-								attributePath.as(String.class),
+								root.get(column),
+								"%" + searchRequestDto.getValue() + "%"
+						);
+						predicateList.add(predicate);
+					}
+
+					case "cpuType",
+							"ram",
+							"hardDisk",
+							"screenSize",
+							"mouseConnectType" -> {
+						predicate = criteriaBuilder.like(
+								root.get("productDetails"),
 								"%" + searchRequestDto.getValue() + "%"
 						);
 						predicateList.add(predicate);
@@ -85,42 +84,36 @@ public class ProductSpecification {
 
 				}
 
-				if (Objects.requireNonNull(requestDto.getGlobalOperator()) == RequestDto.GLOBAL_OPERATOR.OR) {
-					return criteriaBuilder.or(predicateList.toArray(new Predicate[0]));
-				}
 			}
 
-			Expression<Number> priceDiscount = criteriaBuilder.quot(
-					criteriaBuilder.prod(root.get("price"), root.get("discountPercentage")),
-					100.0
+			Expression<Number> priceDiscount = criteriaBuilder.diff(
+					root.get("price"),
+					criteriaBuilder.quot(
+							criteriaBuilder.prod(root.get("price"), root.get("discountPercentage")),
+							100.0
+					)
 			);
 
 			// SORT
 			if (requestDto.getSortDirection() == RequestDto.SORT_DIRECTION.ASC) {
-				switch (requestDto.getSortColumn()) {
+				switch (requestDto.getSortColumn().toLowerCase()) {
 					case "sold", "inserted_time" ->
 							query.orderBy(criteriaBuilder.asc(stockJoin.get(requestDto.getSortColumn())));
 
 					case "price" -> query.orderBy(
 							criteriaBuilder.asc(
-									criteriaBuilder.diff(
-											root.get("price"),
-											priceDiscount
-									)
+									priceDiscount
 							)
 					);
 				}
 			} else {
-				switch (requestDto.getSortColumn()) {
+				switch (requestDto.getSortColumn().toLowerCase()) {
 					case "sold", "inserted_time" ->
 							query.orderBy(criteriaBuilder.desc(stockJoin.get(requestDto.getSortColumn())));
 
 					case "price" -> query.orderBy(
 							criteriaBuilder.desc(
-									criteriaBuilder.diff(
-											root.get("price"),
-											priceDiscount
-									)
+									priceDiscount
 							)
 					);
 				}
