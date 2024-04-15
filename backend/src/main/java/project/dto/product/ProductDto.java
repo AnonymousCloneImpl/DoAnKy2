146cd7e50aff258b4e43ddb4bcbca8d2ea12dc3f
huunghiadvc <mos98p@gmail.com>
2024-04-15 08:25:56 +0700

    update docker-compose

--- backend/src/main/java/project/dto/product/ProductDto.java
+++ backend/src/main/java/project/dto/product/ProductDto.java
@@ -1,6 +1,7 @@
 package project.dto.product;
 
 import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 import java.util.List;
@@ -11,19 +12,20 @@ import java.util.Map;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash("product")
 public class ProductDto implements Serializable {
-    private long id;
-    private String producer;
-    private String model;
-    private String name;
-    private String type;
-    private String details;
-    private Double price;
-    private List<String> imageList;
-    private byte discountPercentage;
-    private BlogDto blog;
-    private PurchaseComboItem purchaseComboItem;
-    private List<SimilarProductDto> similarProductList;
-    private StockDto stock;
-    private Map<String, String> configurationMap;
+	private long id;
+	private String producer;
+	private String model;
+	private String name;
+	private String type;
+	private String details;
+	private Double price;
+	private List<String> imageList;
+	private byte discountPercentage;
+	private BlogDto blog;
+	private PurchaseComboItem purchaseComboItem;
+	private List<SimilarProductDto> similarProductList;
+	private StockDto stock;
+	private Map<String, String> configurationMap;
 }
